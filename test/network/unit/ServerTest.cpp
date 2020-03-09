b2382f3b159ebea2338c5dae594a9123d1470984
João Comini <joaocomini@gmail.com>
2020-03-09 19:25:42 -0300

    fix: removed server test

--- test/network/unit/ServerTest.cpp
+++ test/network/unit/ServerTest.cpp
@@ -3,36 +3,9 @@
 
 using lumina::network::Server;
 
-TEST(ServerTest, CanBuild) {
-    Server server = Server::Builder()
-        .setChannels(2)
-        .setConnections(10)
-        .setPort(8883)
-        .setTickRate(10)
-        .build();
+TEST(ServerTest, UniquePort) {
+    
 
-    EXPECT_EQ(server.getChannels(), 2);
-    EXPECT_EQ(server.getConnections(), 10);
-    EXPECT_EQ(server.getAddress().port, 8883);
-    EXPECT_EQ(server.getTickRate(), 10);
-}
-
-TEST(ServerTest, CanBuildPointer) {
-    Server * server = Server::Builder()
-        .setChannels(2)
-        .setConnections(10)
-        .setPort(12312)
-        .setTickRate(10)
-        .buildPtr();
-
-    EXPECT_TRUE(server != nullptr);
-
-    EXPECT_EQ(server->getChannels(), 2);
-    EXPECT_EQ(server->getConnections(), 10);
-    EXPECT_EQ(server->getAddress().port, 12312);
-    EXPECT_EQ(server->getTickRate(), 10);
-
-    delete server;
 }
 
 
