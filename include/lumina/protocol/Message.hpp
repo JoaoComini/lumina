b7f97b2a346db8286c5b3be8b336fd920b778cfd
João Comini <joaocomini@gmail.com>
2020-03-13 01:32:49 -0300

    feature: created protocol layer

---
+++ include/lumina/protocol/Message.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace lumina{  
+namespace protocol {
+
+    struct Message
+    {
+        uint8_t type;
+    };
+
+}  // namespace net
+} // namespace lumina
+
---
+++ include/lumina/protocol/MessageFactory.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+
+#include "Message.hpp"
+
+namespace lumina {
+namespace protocol {
+
+    class MessageFactory
+    {
+        public:
+            virtual ~MessageFactory() = default;
+            virtual Ref<Message> create(uint8_t type) = 0;
+    };
+    
+} // namespace net
+} // namespace lumina
